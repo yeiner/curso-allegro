0f04c85897b76e5b6d396d5f1975e2cabaf4d50f
Yeiner <yeiner.morales@gmail.com>
2019-08-29 20:39:11 -0500

    Avance curso

--- arka.cpp
+++ arka.cpp
@@ -13,6 +13,17 @@
 #define alto 740
 
 int retardo =100;
+int vida=3;
+int level=1;
+int score=0;
+bool juegoIniciado=false;
+bool fin = false;
+bool nuevoNivel=false;
+int dirY=1;
+int dirX=1;
+int velocidad=3;
+int velocidadInicial=3;
+int fondoN=1;
 
 using namespace std;
 
