3949d5718d613c1da776c4b70a70fea69ef73a93
Yeiner <yeiner.morales@gmail.com>
2019-08-29 20:28:49 -0500

    First commit

---
+++ arka.cpp
@@ -0,0 +1,24 @@
+#include <allegro.h>
+#include <string>
+#include <conio.h>
+#include <sstream>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <winalleg.h>
+#include <fstream>
+
+#define ancho 1024
+#define alto 740
+
+int retardo =100;
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+END_OF_MAIN();
